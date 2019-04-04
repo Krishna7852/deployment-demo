e735cc445db78476fc237c04bee72894dcfd2a08
krishna <krushnabhamare@gmail.com>
2019-04-04 20:35:32 +0530

    Updates

---
+++ precache-manifest.6129b2f65de173ec63ba742733387ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/deployment-demo/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "4b8f57421e5cc03501b7",
+    "url": "/deployment-demo/static/js/runtime~main.fd836547.js"
+  },
+  {
+    "revision": "361b575bbb36a4dd3681",
+    "url": "/deployment-demo/static/js/main.828139d8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/deployment-demo/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "361b575bbb36a4dd3681",
+    "url": "/deployment-demo/static/css/main.889ba26e.chunk.css"
+  },
+  {
+    "revision": "681c4dc35b8179a57eae9cd90ee9490d",
+    "url": "/deployment-demo/index.html"
+  }
+];
